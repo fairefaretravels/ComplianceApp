806d7b903ea4c3996712d86590b357bdee7b329f
SANECA MACK <SANECATV@GMAIL.COM>
2026-06-19 16:42:52 -0400

    Delete src/App.test.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders the Field Ops header", () => {
-  render(<App />);
-  const header = screen.getByText(/field ops/i);
-  expect(header).toBeInTheDocument();
-});
